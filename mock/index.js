584ad4db614d5ad37000c2de45fb3ef9faced79a
DevSergioGC <sergiocamachog12@gmail.com>
2023-12-16 02:29:30 -0400

    test(db): mock db
    Add querys to populate the db for test cases

---
+++ mock/index.js
@@ -0,0 +1,7 @@
+const Clients = require('./clients.mock.query');
+const Certificates = require('./certificates.mock.query');
+const TransactionType = require('./transactionType.mock.query');
+
+Clients();
+Certificates();
+TransactionType();
